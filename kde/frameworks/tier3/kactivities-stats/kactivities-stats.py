bad04a9ad7af6bb7160eed8619d56de91f4589f0
Robert Lancaster <rlancaste@gmail.com>
2020-12-26 15:56:22 -0500

    updating frameworks

--- kde/frameworks/tier3/kactivities-stats/kactivities-stats.py
+++ kde/frameworks/tier3/kactivities-stats/kactivities-stats.py
@@ -4,20 +4,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-
-        self.description = "Configuration system for KNotify"
+        self.description = "A library for accessing the usage data collected by the activities system"
+        self.patchToApply["5.74.0"] = [("kactivities-stats-5.74.0-20200914.diff", 1)]
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["libs/qt5/qtbase"] = None
-        self.runtimeDependencies["dev-utils/snoretoast"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kcompletion"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kservice"] = None
-
+        self.runtimeDependencies["kde/frameworks/tier3/kactivities"] = None
+        self.runtimeDependencies["libs/boost/boost-headers"] = None
 
 from Package.CMakePackageBase import *
 
--- kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
+++ kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
@@ -5,20 +5,18 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
-        self.description = "Configuration system for KNotify"
+        self.description = "QtQuickControls 2 style that integrates with the desktop"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["libs/qt5/qtbase"] = None
-        self.runtimeDependencies["dev-utils/snoretoast"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kcompletion"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kservice"] = None
-
-
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
+        
 from Package.CMakePackageBase import *
 
 
